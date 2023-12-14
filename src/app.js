c85b769a52fdd5f1b8136ad0269bf401595e5f98
Ruangdech  Kurunat <ruangdech@gmail.com>
2023-12-14 15:08:11 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 var _ = require('lodash');
-var util = require('./util.js');
+var util = require('./util/util.js');
+var restSrv = require('./service/rest.js');
 
 main();
 
 async function main() {
   console.log('---- Begin ----');
 
-  const inputData = await util.readJsonFile('./data/data_user.json').then((data) => {
+  //* Read data from File
+  // const inputData = await util.readJsonFile('./data/data_user.json').then((data) => {
+  //   return data;
+  // });
+
+  //* Read data from API
+  const inputData = await restSrv.getUserAll().then((data) => {
     return data;
   });
 
---
+++ src/service/rest.js
@@ -0,0 +1,18 @@
+var app_cont = require('./../cont/app_cont.js');
+
+async function getUserAll() {
+  url = app_cont.REST_BASE_URL + '/users/search?startAt=0&maxResults=0';
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: app_cont.AUTHORIZATION,
+      'Content-Type': 'application/json',
+    },
+  });
+  const result = await response.json();
+  //   console.log(result);
+  return result;
+}
+
+module.exports = { getUserAll };

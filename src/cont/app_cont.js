c502d868ca597de4b28606edd17de73e17c85aa0
Ruangdech  Kurunat <ruangdech@gmail.com>
2024-11-11 23:39:46 +0700

    update REST_BASE_URL to point to new Jira instance

--- src/cont/app_cont.js
+++ src/cont/app_cont.js
@@ -1,3 +1,3 @@
-const REST_BASE_URL = 'https://jira-sense-info-tech.atlassian.net/rest/api/3';
+const REST_BASE_URL = 'https://ruangdech.atlassian.net/rest/api/3';
 
 module.exports = { REST_BASE_URL };
